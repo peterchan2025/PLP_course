256d9da6bc9fbc7b6148b578dbb9e067605c8eec
Peter Chan <chanpeter3858@gmail.com>
2025-08-05 11:42:45 +0300

    Initial clean upload of PLP course

---
+++ Python_module/Index.py
@@ -0,0 +1,3 @@
+basket = 10
+bucket = "water"
+print("This class is crazy!")
---
+++ Python_module/greetings.py
@@ -0,0 +1,13 @@
+print("Hell world")
+
+# ways of commenting
+    #using # sign
+    # using """ commented text """ - docstring
+
+"""
+ways of running your python file in vs code
+1. Click on play button on top right
+2. Right click inside the file, and select run python file on terminal
+3. type the file name: python greetings.py
+
+"""
